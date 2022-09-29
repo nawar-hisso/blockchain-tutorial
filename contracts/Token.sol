55b8c957d3174a266311100079d7540bf87e94a3
Nawar Hisso <nawar@polytrade.finance>
2022-09-29 22:24:54 +0400

    :sparkles: Change Token Constructor

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,14 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./ERC20.sol";
 
 contract Token is ERC20 {
-    string private _name = "Nawar";
-    string private _symbol = "NWR";
     uint256 private _totalSupply = 1000000 * 10**uint(decimals());
 
-    constructor() ERC20(_name, _symbol) {
+    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {
         _mint(msg.sender, _totalSupply);
     }
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,13 +1,23 @@
 const hre = require('hardhat');
+const fs = require('fs');
 
 async function main() {
+  const name = 'Nawar';
+  const symbol = 'NWR';
   // Deploy the token contract
   const Token = await hre.ethers.getContractFactory('Token');
-  const token = await Token.deploy();
+  const token = await Token.deploy(name, symbol);
   // QUESTION: Why is the following statement?
   await token.deployed();
   const tokenAddress = token.address;
+  const tokenABI = JSON.parse(token.interface.format('json'));
+  const data = {
+    address: tokenAddress,
+    abi: tokenABI,
+  };
   console.log(`Token address: ${tokenAddress}`);
+  console.log(`Token ABI: ${JSON.stringify(tokenABI)}`);
+  //fs.writeFileSync('Nawar.json', JSON.stringify(data));
 }
 
 main().catch(error => {
