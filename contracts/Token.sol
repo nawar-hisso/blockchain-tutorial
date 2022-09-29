8a1a2f8cda31d9b5908dafeaadbb2fba89e9a9d6
Nawar Hisso <nawar@polytrade.finance>
2022-09-29 22:27:31 +0400

    :sparkles: Change Token Constructor

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-import "./ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
     uint256 private _totalSupply = 1000000 * 10**uint(decimals());
